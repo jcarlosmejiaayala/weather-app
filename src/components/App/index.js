82bf65bed0770f894272fe3904515d063358a13f
Juan Mejia <jmejia@foundationmedicine.com>
2018-05-14 23:11:21 -0500

    remove consumer from app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 
-import Store from 'components/Store'
+import Provider from 'components/Store'
 
 const App = ({children}) => (
-    <Store.Provider>
-        <Store.Consumer>
-            {children}
-        </Store.Consumer>
-    </Store.Provider>
+    <Provider>
+        {children}
+    </Provider>
 )
 
 export default App
