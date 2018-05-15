8d79f9aadb0b0f26e2f0648694535b649192a708
Juan Mejia <jmejia@foundationmedicine.com>
2018-05-15 00:15:31 -0500

    add methods and values to default state

--- src/components/Store/index.js
+++ src/components/Store/index.js
@@ -1,6 +1,18 @@
 import React, {Component, createContext} from 'react'
 
-const DEFAULT_STATE = {}
+import {getCurrentWeatherService, getForecastWeatherService} from 'lib/apiService'
+
+const DEFAULT_STATE = {
+    currentWeater: {
+        current: null,
+        location: null
+    },
+    forecastWeather: {
+        current: null,
+        location: null,
+        forecast: null
+    }
+}
 
 const Context = createContext(DEFAULT_STATE)
 
@@ -17,9 +29,36 @@ export const Consumer = Mixin => class extends Component {
 export default class Provider extends Component {
     state = DEFAULT_STATE
 
+    getCurrentWeather = async() => {
+        const {current, location} = await getCurrentWeatherService()
+
+        this.setState({
+            currentWeater: {
+                current,
+                location
+            }
+        })
+
+    }
+
+    getForecastWeather = async() => {
+        const {current, location, forecast} = await getForecastWeatherService()
+
+        this.setState({
+            forecastWeather: {
+                current,
+                location,
+                forecast
+            }
+        })
+    }
+
     render() {
         return (
-            <Context.Provider value={{
+            <Context.Provider
+                value={{
+                getCurrentWeather: this.getCurrentWeather,
+                getForecastWeather: this.getForecastWeather,
                 ...this.state
             }}>
                 {this.props.children}
